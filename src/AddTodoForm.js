091eb30430539f014308ce972383f06b02e232bc
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-10-30 23:36:49 -0400

    Move Todo List into State, Control Add Todo Input, Add New Todo to List, Destructure Props

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,17 +1,28 @@
 import React from 'react';
-let AddTodoForm = (props) => {
+
+
+
+let AddTodoForm = ({ onAddTodo }) => {
+    const [todoTitle, setTodoTitle] = React.useState([]);
+    
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    }
     const handleAddTodo = (event) => {
         event.preventDefault()
-        const todoTitle = event.target.elements.title.value;
+        
         console.log(`${todoTitle}`);
+        setTodoTitle("");
         event.target.elements.title.value = "";
-        props.onAddTodo(todoTitle);
+        const newTodoObj = { id: Date.now(), title: todoTitle };
+        onAddTodo(newTodoObj);
     }
     
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Title</label>
-            <input id="todoTitle" name="title" ></input>
+            <input id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange} ></input>
             <button>Add</button>
         </form>
     );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-    {
-        id: 1,
-        title: 'Complete assignment',
-    },
-    {
-        id: 2,
-        title: 'Complete mindset assignment',
-    },
-    {
-        id: 3,
-        title: 'Submit assignment',
-    }
-];
+// const todoList = [
+//     {
+//         id: 1,
+//         title: 'Complete assignment',
+//     },
+//     {
+//         id: 2,
+//         title: 'Complete mindset assignment',
+//     },
+//     {
+//         id: 3,
+//         title: 'Submit assignment',
+//     }
+// ];
 
-let TodoList = () => {
+let TodoList = ({ todoList }) => {
     return (
         <ul>
             {todoList.map(function (item) {
-                return (<TodoListItem key={item.id} todo={item} />);
+                return (<TodoListItem key={item} todo={item} />);
             })}
         </ul>
 )    
