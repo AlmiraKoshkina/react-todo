c573554f4d2969565fdb2f687564cc836dc6a4af
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-11-06 15:43:00 -0500

    Save Todo List in Storage and Create Custom Hook

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-
-
-function App() {
-  // const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")));
-  const [todoList, setTodoList] = React.useState([]);
-  const addTodo = (newTodo) => {
-    const todos = [...todoList, newTodo];
-    setTodoList(todos);
-  }
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList"))) || []
+  // const [todoList, setTodoList] = React.useState([]); 
   
   React.useEffect(() => {
     console.log('todoList has changed:', todoList);
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
   }, [todoList]);
+  
+  return [todoList, setTodoList]
+}
 
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
+  const addTodo = (newTodo) => {
+    const todos = [...todoList, newTodo];
+    setTodoList(todos);
+  }
+  
+  
   return (
     <div>
       <h1>Todo List</h1>
