93865d33f8db37341db7d09210320696102529b2
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-11-05 23:11:45 -0500

    Save Todo List in Storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
-// import { useState } from "react";
+
 
 
 function App() {
-  // const [newTodo, setNewTodo] = React.useState('');
+  // const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")));
   const [todoList, setTodoList] = React.useState([]);
   const addTodo = (newTodo) => {
     const todos = [...todoList, newTodo];
     setTodoList(todos);
   }
+  
+  React.useEffect(() => {
+    console.log('todoList has changed:', todoList);
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
 
   return (
     <div>
       <h1>Todo List</h1>
-      {/* <AddTodoForm onAddTodo={setNewTodo} /> */}
+      
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-      {/* <p>{newTodo}</p>     */}
+      
     </div>
   );
 }
