4a8aed7b1ca7d232903d61541e925616b4f68d92
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-11-06 15:50:48 -0500

    Fix getItem

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList"))) || []
-  // const [todoList, setTodoList] = React.useState([]); 
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")) || [])
   
   React.useEffect(() => {
     console.log('todoList has changed:', todoList);
