69d7bb1f27c21a3686e624fdc07dff9d394dbd98
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-11-15 00:14:38 -0500

    Fragments

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ function App() {
   
   
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
       
-    </div>
+    </>
   );
 }
 
