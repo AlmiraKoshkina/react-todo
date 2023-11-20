cdf87942ebd10f5f714e44e06d9fe4dd6c403dc7
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-11-20 01:54:57 -0500

    Remove Custom Hook

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")) || [])
-  
-  React.useEffect(() => {
-    console.log('todoList has changed:', todoList);
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
-  
-  return [todoList, setTodoList]
-}
+
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
   
+  
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")) || [])
+  React.useEffect(() => {
+      console.log('todoList has changed:', todoList);
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }, [todoList]);
+
   const addTodo = (newTodo) => {
     const todos = [...todoList, newTodo];
     setTodoList(todos);
