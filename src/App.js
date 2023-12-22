0b04afdf11e2f429665496c02dec887818670f0d
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-12-22 01:12:37 -0500

    method  POST

--- src/App.js
+++ src/App.js
@@ -7,80 +7,80 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   
   
-  const [todoList, setTodoList] = React.useState([])
+  // const [todoList, setTodoList] = React.useState([])
 
-  const [isLoading, setIsLoading] = React.useState(true);
+  // const [isLoading, setIsLoading] = React.useState(true);
 
 
-  const fetchData = async () => {
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`
-    const options = {
-      method: "GET",
-      headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`
-      }
-    };
+  // const fetchData = async () => {
+  //   const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`
+  //   const options = {
+  //     method: "GET",
+  //     headers: {
+  //       'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`
+  //     }
+  //   };
 
-    try {
+  //   try {
 
-      const response = await
-        fetch(url, options);
+  //     const response = await
+  //       fetch(url, options);
 
-      if (!response.ok) {
-        const message = `Error: ${response.status}`;
-        throw new Error(message);
-      }
+  //     if (!response.ok) {
+  //       const message = `Error: ${response.status}`;
+  //       throw new Error(message);
+  //     }
       
-      const data = await response.json();
-      console.log(data);
-      const todos = data.records.map((todo) => {
+  //     const data = await response.json();
+  //     console.log(data);
+  //     const todos = data.records.map((todo) => {
       
-        const newTodo = {
-          id: todo.id,
-          title: todo.fields.title
-        }
-        return newTodo
+  //       const newTodo = {
+  //         id: todo.id,
+  //         title: todo.fields.title
+  //       }
+  //       return newTodo
 
-      });
+  //     });
 
-      console.log(todos);
-      setTodoList(todos);
-      setIsLoading(false)
+  //     console.log(todos);
+  //     setTodoList(todos);
+  //     setIsLoading(false)
 
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
+  //   } catch (error) {
+  //     console.log(error.message)
+  //   }
+  // }
 
-  React.useEffect(() => {
-      fetchData()
-  }, []); 
+  // React.useEffect(() => {
+  //     fetchData()
+  // }, []); 
   
   
-  React.useEffect(() => {
-      console.log('todoList has changed:', todoList);
+  // React.useEffect(() => {
+  //     console.log('todoList has changed:', todoList);
     
     
-    if (!isLoading) {
+  //   if (!isLoading) {
      
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    }
-  }, [todoList, isLoading]);
+  //     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  //   }
+  // }, [todoList, isLoading]);
 
  
 
-  const addTodo = (newTodo) => {
-    const todos = [...todoList, newTodo];
-    setTodoList(todos);
-  }
+  // const addTodo = (newTodo) => {
+  //   const todos = [...todoList, newTodo];
+  //   setTodoList(todos);
+  // }
 
-  const removeTodo = (id) => {
-    const index = todoList.findIndex(obj => obj.id === id);
-    const newTodoList = todoList.filter((todo) => todo.id !== id)
-    setTodoList(newTodoList)
-    console.log("Removing item with id: " + id + " at index: " + index);
+  // const removeTodo = (id) => {
+  //   const index = todoList.findIndex(obj => obj.id === id);
+  //   const newTodoList = todoList.filter((todo) => todo.id !== id)
+  //   setTodoList(newTodoList)
+  //   console.log("Removing item with id: " + id + " at index: " + index);
         
-  }
+  // }
   
     
   return (
--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function Main () {
+function Main() {
 
 
     const [todoList, setTodoList] = React.useState([])
@@ -10,27 +10,48 @@ function Main () {
     const [isLoading, setIsLoading] = React.useState(true);
 
 
-    React.useEffect(() => {
-        const myPromise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                const result = {
-                    data: { todoList: JSON.parse(localStorage.getItem("savedTodoList") || []) }
-                };
+    const fetchData = async () => {
+        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`
+        const options = {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`
+            }
+        };
 
-                resolve(result);
-            }, 2000);
-        })
+        try {
 
-        myPromise
-            .then(result => {
-                setTodoList(result.data.todoList);
-                setIsLoading(false);
-            })
-            .catch(error => {
-                console.error(error);
+            const response = await
+                fetch(url, options);
+
+            if (!response.ok) {
+                const message = `Error: ${response.status}`;
+                throw new Error(message);
+            }
+
+            const data = await response.json();
+            console.log(data);
+            const todos = data.records.map((todo) => {
+
+                const newTodo = {
+                    id: todo.id,
+                    title: todo.fields.title
+                }
+                return newTodo
 
             });
 
+            console.log(todos);
+            setTodoList(todos);
+            setIsLoading(false)
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    React.useEffect(() => {
+        fetchData()
     }, []);
 
 
@@ -46,33 +67,64 @@ function Main () {
 
 
 
-    const addTodo = (newTodo) => {
-        const todos = [...todoList, newTodo];
-        setTodoList(todos);
-    }
+    // const addTodo = (newTodo) => {
+    //     const todos = [...todoList, newTodo];
+    //     setTodoList(todos);
+    // }
+
+    const postTodo = async (todo) => {
+        try {
+            const airtableData = {
+                fields: {
+                    title: todo.title,
+                },
+            };
+
+            const response = await fetch(
+                `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+                    },
+                    body: JSON.stringify(airtableData)
+                }
+            );
+
+            if (!response.ok) {
+                const message = `Error has ocurred:
+                             ${response.status}`;
+                throw new Error(message);
+            }
+
+            const dataResponse = await response.json();
+            return dataResponse;
+        } catch (error) {
+            console.log(error.message);
+            return null;
+        }
+    };
 
     const removeTodo = (id) => {
         const index = todoList.findIndex(obj => obj.id === id);
         const newTodoList = todoList.filter((todo) => todo.id !== id)
         setTodoList(newTodoList)
         console.log("Removing item with id: " + id + " at index: " + index);
-
     }
 
-
     return (
-        
-                    <>
+        <>
 
-                        <h1>Todo List</h1>
+            <h1>Todo List</h1>
 
-                        <AddTodoForm onAddTodo={addTodo} />
-                        {isLoading ? <p>Loading...</p> : <div><TodoList todoList={todoList} onRemoveTodo={removeTodo} /></div>}
+            <AddTodoForm onAddTodo={postTodo} />
+            {isLoading ? <p>Loading...</p> : <div><TodoList todoList={todoList} onRemoveTodo={removeTodo} /></div>}
 
-                    </>
-                
+
+        </>
+
     );
 }
-
 
 export default Main;
