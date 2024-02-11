662bbe0a0c0b1ffdfa16bfab93b0c865ab68b145
AlmiraKoshkina <almira.koshkina@gmail.com>
2024-02-11 00:44:44 -0500

    Sort by Airtable field

--- src/Main.js
+++ src/Main.js
@@ -12,7 +12,7 @@ function Main() {
 
 
     const fetchData = async () => {
-        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?view=Grid%20view`
+        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`
         const options = {
             method: "GET",
             headers: {
